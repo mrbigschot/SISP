d43308bc4cdfcd3ea84f0f77a2f437f241071fa5
Paul Schot <pks5885@gmail.com>
2017-02-28 13:24:41 -0800

    Better debug/test structure.
    That's all.

--- src/schotswarmintelligence/Swarm.java
+++ src/schotswarmintelligence/Swarm.java
@@ -31,24 +31,24 @@ public class Swarm extends ArrayList<Bug> {
     }
 
     private void testInit() {
-//        Bug b1 = new Bug(this, 250, 250);
-//        b1.setAngle(3.5);
-//        b1.setSpeed(1.0);
-//        b1.index = 1;
-//        this.add(b1);
-//        Bug b2 = new Bug(this, 260, 250);
-//        b2.setAngle(2);
-//        b2.setSpeed(1.4);
-//        b2.index = 2;
-//        this.add(b2);
-        for (int i = 0; i < 20; i++) {
-                int x = Globals.random(200, 300);
-                int y = Globals.random(200, 300);
-                Bug bug = new Bug(this, x, y);
-                bug.setAngle(0);
-                bug.setSpeed(1);
-                this.add(bug);
-            }
+        Bug b1 = new Bug(this, 250, 250);
+        b1.setAngle(Math.PI);
+        b1.setSpeed(1.0);
+        b1.index = 1;
+        this.add(b1);
+        Bug b2 = new Bug(this, 253, 250);
+        b2.setAngle(0.0);
+        b2.setSpeed(1.0);
+        b2.index = 2;
+        this.add(b2);
+//        for (int i = 0; i < 20; i++) {
+//            int x = Globals.random(200, 300);
+//            int y = Globals.random(200, 300);
+//            Bug bug = new Bug(this, x, y);
+//            bug.setAngle(0);
+//            bug.setSpeed(1);
+//            this.add(bug);
+//        }
     }
 
     public void updateLocation(int x, int y) {
